d5491ae7dec2717d5123e802c233a72441ddc6ef
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-04-27 19:02:54 +0200

    Site cleaned up

--- src/actions/SendEmail.js
+++
@@ -1,7 +0,0 @@
-"use server"
-
-import React from "react"
-import {Resend} from "resend"
-import ContactFormEmail from "@/email/ContactFormEmail"
-
-const resend = new Resend(process.env.RESEND_API_KEY)
