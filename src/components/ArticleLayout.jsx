12467baa4d012b03daae2ea4c93f584520a69123
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-04-27 20:05:56 +0200

    fixed deployment bugs

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -1,5 +1,5 @@
 import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 import { Container } from '@/components/Container'
 import { formatDate } from '@/lib/formatDate'
